5e45f376dae32882112cc6305337d86975dab501
rohitrango <rohit.rango@gmail.com>
2019-01-21 23:09:32 +0530

    Broken Q1

--- Assignment 1/Q1/code/angles2rot.m
+++ Assignment 1/Q1/code/angles2rot.m
@@ -2,6 +2,27 @@ function rot_matrix = angles2rot(angles_list)
     %% Your code here
     % angles_list: [theta1, theta2, theta3] about the x,y and z axes,
     % respectively.
+    N = size(angles_list, 1);
+    rot_matrix = zeros(N, 3, 3);
+    for i=1:N,
+        % Find angles 
+        wx = angles_list(i, 1);
+        wy = angles_list(i, 2);
+        wz = angles_list(i, 3);
+        Rx = [  1, 0, 0;
+                0, cos(wx), -sin(wx); 
+                0, sin(wx), cos(wx)
+             ];
+        Ry = [  cos(wy), 0, sin(wy);
+                0, 1, 0; 
+               -sin(wy), 0, cos(wy)
+             ];
+        Rz = [  cos(wz), -sin(wz), 0;
+                sin(wz), cos(wz), 0;
+                0, 0, 1;
+             ];
+        rot_matrix(i, :, :) = Rz*Ry*Rx;
+    end
 end
 
 
--- Assignment 1/Q1/code/transformPose.m
+++ Assignment 1/Q1/code/transformPose.m
@@ -3,5 +3,40 @@ function [result_pose, composed_rot] = transformPose(rotations, pose, kinematic_
     % pose: The base pose coordinates 16 x 3.
     % kinematic chain: A 15 x 2 array of joint ordering
     % root_positoin: the index of the root in pose vector.
-    % Your code here 
+    % Your code here
+    
+    % Composite rotation maintained here, one for each point
+    composed_rot = zeros(size(pose, 1), 3, 3);
+    for i=1:size(composed_rot, 1),
+        composed_rot(i, :, :) = eye(3);
+    end
+    
+    % Bone indices to initialize bones
+    boneIndices = size(kinematic_chain, 1);    
+    
+    % Initialize the composite roations first
+    for boneIndex = 1:boneIndices,
+       % Given a bine rotation, the child is rotated
+       childIndex = kinematic_chain(boneIndex, 1);
+       parentIndex = kinematic_chain(boneIndex, 2);
+       composed_rot(childIndex, :, :) = squeeze(rotations(boneIndex, :, :))*squeeze(composed_rot(parentIndex, :, :));
+    end
+
+    % Initialized the root joint
+    result_pose = zeros(16, 3);
+    result_pose(root_location, :) = pose(root_location, :);
+    
+    for boneIndex = 1:boneIndices,
+       % For every joint, we need to find parent, child
+       childIndex  = kinematic_chain(boneIndex, 1);
+       childCoord  = pose(childIndex, :);
+       
+       parentIndex = kinematic_chain(boneIndex, 2);
+       parentCoord = pose(parentIndex, :);
+     
+       % Now find the new child coord
+       newChildCoord = squeeze(composed_rot(childIndex, :, :))*(childCoord - parentCoord)' + result_pose(parentIndex, :)';
+       result_pose(childIndex, :) = newChildCoord';
+    end
+    
 end
