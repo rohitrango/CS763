73cc9170b1b334345bff2f730d8096558cb8a6bb
Shriram <sbshriram16@gmail.com>
2019-02-07 22:30:10 +0530

    A2-Q1 add function for decomposing projection matrix

---
+++ Assignment 2/Q1/code/decomposeProjectionMatrix.m
@@ -0,0 +1,11 @@
+
+function [K, R, T] = decomposeProjectionMatrix(P)
+%DECOMPOSEP
+    H_inf = P(1 : 3, 1 : 3);
+    [R_t, K_inv] = qr(inv(H_inf));
+    R_z_pi = [-1 0 0; 0 -1 0; 0 0 1];
+    R = R_z_pi * transpose(R_t);
+    K = inv(K_inv) * R_z_pi;
+    T = -1 * inv(K * R) * P(1 : 3, 4);
+end
+
