7b346054f586339b0c1b4bf6ac1380a01b7fd034
Shriram <sbshriram16@gmail.com>
2019-03-24 07:21:41 +0530

    A4 - testing multilayer RNN

---
+++ Assignment 4/config/multilayer_config.py
@@ -0,0 +1,18 @@
+"""
+Specifies default parameters used for training in a dictionary
+"""
+
+config = {}
+
+dataset = {}
+dataset['seq_max_len'] = 3000
+dataset['num_classes'] = 2
+dataset['pad_beginning'] = True
+dataset['truncate_end'] = True
+
+network = {}
+network['hidden_size'] = 64
+network['num_layers'] = 2
+network['cell_type'] = 'RNN'
+
+config = {'dataset': dataset, 'network': network}
